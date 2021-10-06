1d806419a3709de41f989023455fc333f1088214
Omar Omran <omaromran2003@gmail.com>
2021-10-07 02:39:02 +0400

    Add route to request objects info (API)

--- website/routes.py
+++ website/routes.py
@@ -1,13 +1,24 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,jsonify
 from website.models import Objects
 from website import app,jsglue
 
-
-@app.route("/home",methods =['GET','POST'])
+@app.route("/home",methods =['GET'])
 @app.route('/raw',methods =['GET'])
 def raw():
-    return render_template('raw.html',enumerate=enumerate,data=Objects.query.all(),title ='Raw Data')
+    '''this route displays the raw data'''
+
+    objects = dict_generator(Objects.query.all())
+    return render_template('raw.html',enumerate=enumerate,data=objects,title ='Raw Data')
 
 @app.route('/model',methods =['GET','POST'])
-def home():
+def model():
+    '''this route displays the model'''
+
     return render_template('model.html')
+
+@app.route('/fetch_objects',methods =['GET','POST'])
+def fetch_objects():
+    '''requests should be sent to this route to get the info of the objects'''
+
+    objects = dict_generator(Objects.query.all())
+    return jsonify(objects)
