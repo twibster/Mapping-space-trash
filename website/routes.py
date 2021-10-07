5567e54f8655151d88cd62fd2131bc429bfe9630
Omar Omran <omaromran2003@gmail.com>
2021-10-07 15:19:52 +0400

    fix undefined error in home view

--- website/routes.py
+++ website/routes.py
@@ -2,6 +2,21 @@ from flask import render_template,redirect,url_for,jsonify
 from website.models import Objects
 from website import app,jsglue
 
+def row2dict(row):
+    '''this function converts a given row of a query to a dictionary'''
+    dict = {}
+    for column in row.__table__.columns:
+        dict[column.name] = str(getattr(row, column.name))
+    return dict
+
+def dict_generator(query):
+    '''this function converts a query into a dictionary'''
+    results={}
+    for idx,row in enumerate(query):
+        row_as_dict =row2dict(row)
+        results[row_as_dict['name']+str(idx)]=row_as_dict
+    return results
+
 @app.route("/home",methods =['GET'])
 @app.route('/raw',methods =['GET'])
 def raw():
